f38c31cd5be21c8244f889b7bcf95803be557ac0
AronBA <72823328+AronBA@users.noreply.github.com>
2021-09-03 11:58:30 +0200

    added Übungen

--- src/W04/Übung2.java
+++ src/W04/Übung2.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class Übung2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Gib eine Zeichenkette an: ");
-        String stringA = sc.nextLine();
-
 
 
 
---
+++ src/W04/Übung3.java
@@ -0,0 +1,15 @@
+package W04;
+
+import java.util.Scanner;
+import java.util.concurrent.LinkedTransferQueue;
+
+public class Übung3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Für welche Zahl möchtest du eine Multiplikationstafel haben? :");
+        int num = sc.nextInt();
+
+
+    }
+}
