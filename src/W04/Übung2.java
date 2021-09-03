9b8ba4736f4cdcb4c30b9e63961e2ca4bc7d4b0d
AronBA <72823328+AronBA@users.noreply.github.com>
2021-09-03 12:51:44 +0200

    added Übungen

--- src/W04/Übung2.java
+++ src/W04/Übung2.java
@@ -3,11 +3,20 @@ package W04;
 import java.util.Scanner;
 
 public class Übung2 {
-    public static void main(String[] args) {
-
-
-
-
-
+    public static void func() {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a text");
+        while (!myObj.hasNext("exit")) {
+            System.out.println(
+                    myObj.hasNextInt() ? "(int) " + myObj.nextInt() :
+                            myObj.hasNextLong() ? "(long) " + myObj.nextLong() :
+                                    myObj.hasNextDouble() ? "(double) " + myObj.nextDouble() :
+                                            myObj.hasNextBoolean() ? "(boolean) " + myObj.nextBoolean() :
+                                                    "(String) " + myObj.next()
+            );
+        }
+    }
+    public static void main(String[] args){
+        func();
     }
 }
