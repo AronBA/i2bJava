d2437ce927d96ca559992bfe9ab49f16f3a24a99
Aron <72823328+AronBA@users.noreply.github.com>
2021-09-03 09:15:40 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# i2b Java
+read for depressi?
---
+++ src/W04/Übung2.java
@@ -0,0 +1,15 @@
+package W04;
+
+import java.util.Scanner;
+
+public class Übung2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gib eine zahl an: ");
+        int numA = sc.nextInt();
+        System.out.println(numA.());
+
+
+
+    }
+}
