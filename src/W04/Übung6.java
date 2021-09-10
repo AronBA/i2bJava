054fedefb5f23cdb622b4bde66c722bde85aaf8e
AronBA <72823328+AronBA@users.noreply.github.com>
2021-09-10 10:16:55 +0200

    added Übung 5 , 6

---
+++ src/W04/Übung6.java
@@ -0,0 +1,32 @@
+package W04;
+
+import java.util.Scanner;
+
+public class Übung6 {
+    public static void main(String[] args) {
+        System.out.println("Gib eine Zahl an");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = 0;
+        for (int i = 1 ; i <= n;) {
+            if (n %i == 0){
+                t++;
+            }
+            i++;
+        }
+        if (t == 2){
+            System.out.println(n + " ist eine Primzahl");
+        }
+        else{
+            System.out.println(n + " ist keine Primzahl");
+        }
+
+    }
+}
+
+
+
+
+
+
+
