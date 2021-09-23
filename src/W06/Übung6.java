b48534fdfab0887453fe2b1cd39cb733e4782554
AronBA <72823328+AronBA@users.noreply.github.com>
2021-09-24 01:31:06 +0200

    finished Übung 6 - 8

--- src/W06/Übung6.java
+++ src/W06/Übung6.java
@@ -1,7 +1,19 @@
 package W06;
 
+import java.util.Scanner;
+
 public class Übung6 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("geben sie ihre Arbeitsstunden an: ");
+        int stunden = sc.nextInt();
+        System.out.println("geben sie ihre Überstunden an: ");
+        int überstunden = sc.nextInt();
+        System.out.println("geben sie ihren stundenlohn an: ");
+        int stundenlohn= sc.nextInt();
+        int regulär =stunden * stundenlohn;
+        int überstundenlohn = (stundenlohn * überstunden / 100)*20 + (überstunden * stundenlohn);
+        int lohn = überstundenlohn + regulär;
+        System.out.println("Sie erhalten " + lohn + " CHF!");
     }
 }
